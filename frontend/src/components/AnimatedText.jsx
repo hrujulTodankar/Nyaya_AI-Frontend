d5a46474a34d74c1b22ab16a743073a34a632c3f
Hrujul <hrujultodankar48@gmail.com>
2026-02-13 10:26:43 +0530

    fix: Ensure animated text gradient is visible

--- frontend/src/components/AnimatedText.jsx
+++ frontend/src/components/AnimatedText.jsx
@@ -3,13 +3,20 @@ import './AnimatedText.css';
 
 const AnimatedText = ({ text, className = '', style = {} }) => {
   return (
-    <h1 className={`animated-text ${className}`} style={style}>
+    <h1 
+      className={`animated-text ${className}`} 
+      style={{
+        ...style,
+        display: 'inline-block'
+      }}
+    >
       {text.split('').map((char, index) => (
         <span
           key={index}
           className="animated-char"
           style={{
-            animationDelay: `${index * 0.05}s`
+            animationDelay: `${index * 0.05}s`,
+            display: 'inline-block'
           }}
         >
           {char === ' ' ? '\u00A0' : char}
